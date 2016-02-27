479694d4619eb2852b1d5994d84fcc6d8e472efa
Habib Rehman <HR@users.noreply.github.com>
2016-02-27 19:28:47 +0000

    Update term.sh
    remove redundant space

--- term.sh
+++ term.sh
@@ -6,7 +6,7 @@
 
 term () {
 	if [ $# -ge 2 ]						# If more than two dirs passed
-		then										# open each dir
+		then						# open each dir
 			for dir in "$@"
 			do
 				open -a Terminal "$dir"
@@ -15,38 +15,38 @@ term () {
 	fi
 
 	if [ -z "$1" ]						# If no dirs passed then open
-		then										# new term ot current dir
+		then						# new term ot current dir
 			open -a Terminal .
-		else
-			case $1 in
-					-s|--source)
-					source ~/.bash_profile
-					return 0
-					;;
-					-s=*|--source=*)
-					source "${1#*=}";
-					return 0
-					;;
-					-e=*|--exec=*)
-					osascript -e 'tell application "Terminal" to do script with command "'"${1#*=}"'"';
-					return 0
-					;;
-					-h|--help)
-					echo "term [-h | --help] [-s script | --source script] [-e command | --exec command] [dir...] -- a Terminal helper utility"
-					echo "With no arguments specified, a new Terminal is opened at the current directory."
-					echo "If multiple directories are specified then it opens each in a new Terminal window."
-					echo "where:"
-					echo "	-h	show this help text"
-					echo "	-s	source ~/.bash_profile"
-					echo " 	-s=[script] source [script]"
-					echo " 	-e=[command] execute [command] in new window at current directory"
-					return 0
-					;;
+	else
+		case $1 in
+			-s|--source)
+			source ~/.bash_profile
+			return 0
+			;;
+			-s=*|--source=*)
+			source "${1#*=}";
+			return 0
+			;;
+			-e=*|--exec=*)
+			osascript -e 'tell application "Terminal" to do script with command "'"${1#*=}"'"';
+			return 0
+			;;
+			-h|--help)
+			echo "term [-h | --help] [-s script | --source script] [-e command | --exec command] [dir...] -- a Terminal helper utility"
+			echo "With no arguments specified, a new Terminal is opened at the current directory."
+			echo "If multiple directories are specified then it opens each in a new Terminal window."
+			echo "where:"
+			echo "	-h	show this help text"
+			echo "	-s	source ~/.bash_profile"
+			echo " 	-s=[script] source [script]"
+			echo " 	-e=[command] execute [command] in new window at current directory"
+			return 0
+			;;
 
-					*)
-						open -a Terminal "$1"
-					;;
-			esac
+			*)
+				open -a Terminal "$1"
+			;;
+		esac
 	fi
 	return 0
 }
